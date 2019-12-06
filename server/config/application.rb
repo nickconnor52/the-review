692b2caac20d93e9776cec78eee2ac8bbd10b841
Nick Connor <nick.connor@onit.com>
2019-12-06 16:21:27 -0600

    Update .env settings and database config

--- server/config/application.rb
+++ server/config/application.rb
@@ -25,6 +25,14 @@ module TheReview
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Register the local development ENV file to the YML file in config
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
