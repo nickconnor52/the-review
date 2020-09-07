3ad70fefd8bf1a908cb0d674baad4e198be4c4c6
Nick Connor <nickconnor52@gmail.com>
2020-09-06 21:07:08 -0500

    add debug

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
 console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
@@ -16,6 +15,7 @@ if (process.env.NODE_ENV === 'development') {
   axios.defaults.baseURL = 'http://test.com/' || ''
 }
 
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
