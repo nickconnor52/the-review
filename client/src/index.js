f87ae5367088dc41655695b5bc67d273ad0da7b0
Nick Connor <nickconnor52@gmail.com>
2020-09-06 21:06:32 -0500

    add debug

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,13 @@ import axios from 'axios'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+console.log(process.env.NODE_ENV)
 if (process.env.NODE_ENV === 'production') {
+  console.log(process.env.API_BASE_URL)
   axios.defaults.baseURL = process.env.API_BASE_URL || ''
+}
+if (process.env.NODE_ENV === 'development') {
+  axios.defaults.baseURL = 'http://test.com/' || ''
 }
 
 // If you want your app to work offline and load faster, you can change
