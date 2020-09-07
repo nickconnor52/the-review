831d00eeb696803f4d8b2375c45347e9f12d7538
Nick Connor <nickconnor52@gmail.com>
2020-09-06 22:50:47 -0500

    Fix typo

--- client/server.js
+++ client/server.js
@@ -20,9 +20,9 @@ const path = require('path');
 let app = express();
 app.use(history());
 app.use(serveStatic(__dirname + "/dist"));
-app.use(express.static(path.join(__dirname, './build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, './build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
 
